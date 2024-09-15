eec73f2303c80e2075fa261450b801c4566da72d
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2024-09-15 09:50:06 -0400

    chore: add initial files for go and docker

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE poc;
+
+CREATE TABLE IF NOT EXISTS games (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  metadata JSON
+);
+
+INSERT INTO games (title, metadata) VALUES ('Kindom Hearts', '{"tags": ["RPG"]}');
+
+INSERT INTO games (title, description, metadata) VALUES ('God of War', 'Franchise reboot', '{"platforms": ["PS4"]}');
+
+INSERT INTO games (title) VALUES ('The Legend of Zelda2');
